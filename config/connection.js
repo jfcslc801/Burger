ade4eca5197db0c1db61cdebde13524be670f12a
jaime franco cerritos <jfcslc@outlook.com>
2018-05-01 23:25:54 -0600

    connection.js updates

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,19 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "cangetin",
+    database: "burgers_db"
+  });
+};
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
